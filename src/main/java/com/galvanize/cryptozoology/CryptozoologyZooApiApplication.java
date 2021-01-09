de561eee688a66efb5088d371b713685d33c5a46
Vyom <vyom.reloaded@gmail.com>
2021-01-09 14:25:35 -0500

    initialize the repository

---
+++ src/main/java/com/galvanize/cryptozoology/CryptozoologyZooApiApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.cryptozoology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptozoologyZooApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptozoologyZooApiApplication.class, args);
+	}
+
+}
